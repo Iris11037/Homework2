f502e5db67aed26adc36c4c7f09de80593aac78c
Ari <ariyni@yandex.ru>
2024-09-08 17:22:49 +0300

    08/09

---
+++ main.py
@@ -0,0 +1,19 @@
+# "1st program"
+print((9 ** 0.5) * 5)
+
+# "2nd program"
+print(9.99 > 9.98 and 1000 != 1000.1)
+
+# "3rd program"
+a = 2 * 2 + 2
+print(a)
+b = 2 * (2 + 2)
+print(b)
+print(a == b)
+
+# "4th program"
+a = float("123.456")
+var = a * 10
+b = int(var)
+print(b % 10)
+
