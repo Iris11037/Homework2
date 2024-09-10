a01e29b6fbceabdfe83cc8cb538b99bb8969b2c3
Ari <ariyni@yandex.ru>
2024-09-10 13:25:17 +0300

    10/09

---
+++ Work3.py
@@ -0,0 +1,6 @@
+Homework = 12
+Hours = 1.5
+Name = "Python"
+Hours_work = Hours / Homework
+
+print("Курс:",Name,",всего задач:",Homework,",затрачено часов:",Hours,",среднее время выполнения",Hours_work)
