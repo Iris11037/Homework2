ee446bdbead161ab685de57a4f8ded10ff7d1d3a
Ari <ariyni@yandex.ru>
2024-10-10 17:18:53 +0300

    10/10

---
+++ module_4_1.py
@@ -0,0 +1,8 @@
+from fake_math import divide as fake_divide
+from true_math import divide as true_divide
+
+fake_divide(69, 3)
+fake_divide(3, 0)
+true_divide(49, 7)
+result = true_divide(15, 0)
+print(result)
